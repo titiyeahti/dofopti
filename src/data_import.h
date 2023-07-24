9702c732cdbdf008918e1a35c4a2d7e1c7ecff36
titiyeahti <thibaut.milhaud@gmail.com>
2023-07-24 18:50:43 +0200

    trying to order things up

--- src/data_import.h
+++ src/data_import.h
@@ -7,44 +7,6 @@
 #include <jsoncpp/json/json.h>
 #include <sqlite3.h>
 
-#define CREATE_PANOS_SQL "CREATE TABLE IF NOT EXISTS panos" \
-	"(id INTEGER PRIMARY KEY, name TEXT UNIQUE NOT NULL, size INTEGER NOT NULL);"
-
-#define CREATE_ITEMS_SQL "CREATE TABLE IF NOT EXISTS items"\
-	"(id INTEGER PRIMARY KEY, name TEXT UNIQUE NOT NULL,"\
-	"id_pano INTEGER, slot TEXT NOT NULL, category TEXT,"\
-	"min_lvl INTEGER, FOREIGN KEY (id_pano) REFERENCES panos(id) "\
-	"ON UPDATE CASCADE ON DELETE SET NULL);"
-
-#define CREATE_BONUSES_SQL "CREATE TABLE IF NOT EXISTS bonuses"\
-	"(id_pano INTEGER NOT NULL, nb_items INTEGER NOT NULL,"\
-	" stat TEXT NOT NULL, value INTEGER NOT NULL,"\
-	"UNIQUE (id_pano, nb_item, stat),"\
-	"FOREIGN KEY (id_pano) REFERENCES panos(id) "\
-	"ON UPDATE CASCADE ON DELETE CASCADE);"
-
-#define CREATE_STATS_SQL "CREATE TABLE IF NOT EXISTS stats(id_item INTEGER, stat TEXT NOT NULL,\
-	"minval INTEGER, maxval INTEGER,"\
-	"UNIQUE (id_item, stat),"\
-	"FOREIGN KEY (id_item) REFERENCES items(id) "\
-	"ON UPDATE CASCADE ON DELETE CASCADE);"
-
-#define CREATE_CONSTS_SQL "CREATE TABLE IF NOT EXISTS consts(id_item INTEGER, stat TEXT, "\
-	"sign INTEGER, value INTEGER,"\
-	"FOREIGN KEY (id_item) REFERRENCES items(id) "\
-	"ON UPDATE CASCADE ON DELETE CASCADE);"
-
-#define DROP_ITEMS_SQL "DROP TABLE IF EXISTS items;"
-#define DROP_PANOS_SQL "DROP TABLE IF EXISTS panos;"
-#define DROP_STATS_SQL "DROP TABLE IF EXISTS stats;"
-#define DROP_BONUSES_SQL "DROP TABLE IF EXISTS bonuses;"
-#define DROP_CONSTS_SQL "DROP TABLE IF EXISTS consts;"
-
-#define INSERT_PANO "INSERT INTO panos VALUES(?, ?, ?);"
-#define INSERT_ITEM "INSERT INTO items VALUES(?, ?, ?, ?, ?, ?);"
-#define INSERT_BONUS "INSERT INTO bonus VALUES(?, ?, ?, ?);"
-#define INSERT_STAT "INSERT INTO stats VALUES(?, ?, ?, ?);"
-
 int create_tables(sqlite3* db);
 
 int drop_tables(sqlite3* db);
--- src/requests.h
+++
@@ -1,8 +0,0 @@
-#define SR_CREATE_ITEM_TABLE "CREATE TABLE item("\
-	"id INTEGER PRIMARY KEY, "\
-	"name TEXT, "\
-	"panoid INTEGER, "\
-	"minlvl INTEGER, "\
-	"isweapon INTEGER)"
-
-#define SR_CREATE_PANO
