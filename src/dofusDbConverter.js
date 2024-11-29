dd4034f94dddde22bdf31ca05baaf8760184ca28
Rhum <lucas.rioust@gmail.com>
2024-11-29 10:39:22 +0100

    test fix fm

--- src/dofusDbConverter.js
+++ src/dofusDbConverter.js
@@ -6,6 +6,8 @@ function SaveStuff(e) {
     t[0] = {};
     for (var a = 0; a < e.BaseCaracs.length; a += 1)
         0 != e.BaseCaracs[a] && (t[0][a] = e.BaseCaracs[a]);
+    
+    t[0][0] = str(parseInt(t[0][0]) - 100)
     t[1] = e.AdditionalPoints,
         t[2] = e.Level,
         t[3] = 0,
