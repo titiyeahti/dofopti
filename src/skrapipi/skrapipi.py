0822eda2d8506bda1eb6d510e526d900ec977f82
lucas <lucas.rioust@gmail.com>
2026-02-19 23:38:50 +0100

    update work bdd

--- src/skrapipi/skrapipi.py
+++ src/skrapipi/skrapipi.py
@@ -9,17 +9,23 @@ url= "https://api.beta.dofusdb.fr/"
 
 #--- The pages to fetch from the dofusDb API ---#
 
-def url_effects(pages_read):
-    return "https://api.beta.dofusdb.fr/effects?$limit=50&$skip=" +str(pages_read*50)
-def url_characteristics(pages_read):
-    return "https://api.beta.dofusdb.fr/characteristics?$limit=50&$skip=" +str(pages_read*50)
-def url_item_types(pages_read):
-    return "https://api.beta.dofusdb.fr/item-types?$limit=50&$skip=" +str(pages_read*50)
-def url_item_sets(pages_read):
-    return "https://api.beta.dofusdb.fr/item-sets?$limit=50&$skip=" +str(pages_read*50)
-def url_items(pages_read, type_id):
-    return "https://api.beta.dofusdb.fr/items?$limit=50&typeId=" + str(type_id) + "&$skip=" +str(pages_read*50)
-
+def base_url(beta=True):
+    if beta : return "https://api.beta.dofusdb.fr/"
+    return "https://api.dofusdb.fr/"
+def url_effects(pages_read, beta=True):
+    return base_url(beta) + "effects?$limit=50&$skip=" +str(pages_read*50)
+def url_characteristics(pages_read, beta=True):
+    return base_url(beta) + "characteristics?$limit=50&$skip=" +str(pages_read*50)
+def url_item_types(pages_read, beta=True):
+    return base_url(beta) + "item-types?$limit=50&$skip=" +str(pages_read*50)
+def url_item_sets(pages_read, beta=True):
+    return base_url(beta) + "item-sets?$limit=50&$skip=" +str(pages_read*50)
+def url_items(pages_read, type_id, beta=True):
+    return base_url(beta) + "items?$limit=50&typeId=" + str(type_id) + "&$skip=" +str(pages_read*50)
+def url_spells(pages_read, class_id, beta=True):
+    return base_url(beta) + "spells?typeId=" + str(class_id) + "&$limit=50&$skip=" +str(pages_read*50)
+def url_spells_levels(pages_read, class_id, beta=True):
+    return base_url(beta) + "spell-levels?spellBreed=" + str(class_id) + "&$limit=50&$skip=" +str(pages_read*50)
 
 tables = [
         "effects", 
@@ -30,26 +36,37 @@ tables = [
         "set_bonuses",
         "item_stats",
         "stat_codes",
-        "slot_codes"
+        "slot_codes",
+        "spell_levels",
+        "spells"
         ]
 
 banned_item_type_id = [114, 169, 273, 274, 275, 276, 277, 279, 280]
+
+#Ids des classes dans l'ordre (sram, sacri, forgelance bizarre. 21=sort neutre)
+class_ids = [1,2,3,451,5,6,7,8,9,10,580,12,13,14,15,16,17,18,2374,21]
+
+#Les effets qu'on s'autorise à stocker (vol de vie, degat, meilleur element, boost sort)
+valid_effects = [91,92,93,94,95,96,97,98,99,100,2822,2828,3293]
 sql_create_commands = [
         #--- Crée les tables auxiliaires ---#
         "CREATE TABLE IF NOT EXISTS effects (id INTEGER PRIMARY KEY, description TEXT);", 
         "CREATE TABLE IF NOT EXISTS characteristics (id INTEGER PRIMARY KEY, keyword TEXT);", 
         "CREATE TABLE IF NOT EXISTS item_types (id INTEGER PRIMARY KEY, name TEXT, superTypeId INTEGER, categoryId INTEGER);",
         "CREATE TABLE IF NOT EXISTS item_sets (id INTEGER PRIMARY KEY, name TEXT, namefr TEXT);",
+        
         #--- Crée la table principale des items, avec des clés vers les auxis ---#
         "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name TEXT, namefr TEXT, level INTEGER, itemTypeId INTEGER, \
         itemSetId INTEGER, criteria TEXT, imgLink TEXT, \
         FOREIGN KEY (itemSetId) REFERENCES item_sets(id) on update cascade on delete cascade , \
         FOREIGN KEY (itemTypeId) REFERENCES item_types(id) on update cascade on delete cascade );",
+        
         #--- Crée la table des stats de panoplie, avec des clés vers les auxis ---#
         "CREATE TABLE IF NOT EXISTS set_bonuses (setItemId INTEGER, nbItems INTEGER, carac INTEGER, val INTEGER, \
         FOREIGN KEY (setItemId) REFERENCES item_sets(id) on update cascade on delete cascade , \
         FOREIGN KEY (carac) REFERENCES characteristics(id) on update cascade on delete cascade , \
         UNIQUE (setItemId, nbItems, carac));",
+        
         #--- Crée la table des stats de base, avec des clés vers les auxis ---#
         "CREATE TABLE IF NOT EXISTS item_stats (itemId INTEGER, carac INTEGER, minval INTEGER, maxval INTEGER, \
                 FOREIGN KEY (itemId) REFERENCES items(id) on update cascade on delete cascade , UNIQUE (itemId, carac) ON CONFLICT IGNORE,\
@@ -57,7 +74,14 @@ sql_create_commands = [
         
         #--- Crée les tables de mapping de stats/slots d'équipements ---#
         "CREATE TABLE IF NOT EXISTS stat_codes (caracId INTEGER, statCode INTEGER);",
-        "CREATE TABLE IF NOT EXISTS slot_codes (typeId INTEGER, slotCode INTEGER);"
+        "CREATE TABLE IF NOT EXISTS slot_codes (typeId INTEGER, slotCode INTEGER);",
+    
+        #--- Crée les tables de spells
+        "CREATE TABLE IF NOT EXISTS spells (id INTEGER PRIMARY KEY, name TEXT, classId INTEGER);",
+        "CREATE TABLE IF NOT EXISTS spell_levels (levelRequirement INTEGER, effect INTEGER, spell INTEGER, critChance INTEGER,\
+            minVal INTEGER, maxVal INTEGER, minCritVal INTEGER, maxCritVal INTEGER,\
+            FOREIGN KEY (effect) REFERENCES effects(id) on update cascade on delete cascade,\
+            FOREIGN KEY (spell) REFERENCES spells(id) on update cascade on delete cascade);"
         ]
 
 sql_insert_effects_command = "INSERT INTO effects VALUES(?, ?);"
@@ -67,20 +91,135 @@ sql_insert_item_sets_command = "INSERT INTO item_sets VALUES(?, ?, ?);"
 sql_insert_items_command = "INSERT INTO items (id, name, namefr, level, itemTypeId, itemSetId, criteria, imgLink) VALUES(?, ?, ?, ?, ?, ?, ?, ?);"
 sql_insert_set_bonuses_command = "INSERT OR IGNORE INTO set_bonuses (setItemId, nbItems, carac, val) VALUES(?, ?, ?, ?);"
 sql_insert_item_stats_command = "INSERT INTO item_stats (itemId, carac, minval, maxval) VALUES(?, ?, ?, ?);"
+sql_insert_spell_levels_command = "INSERT INTO spell_levels (levelRequirement, effect, spell, critChance, minVal, maxVal, minCritVal, maxCritVal) VALUES(?, ?, ?, ?, ?, ?, ?, ?);"
+sql_insert_spells_command = "INSERT INTO spells (id, name, classId) VALUES(?, ?, ?);"
 
 sql_drop_all_commands = ["DROP TABLE IF EXISTS " + table + ";" for table in tables]
-
+def sql_drop_command(table):
+    return "DROP TABLE IF EXISTS " + table + ";"
 
 #import order  : 
 #1 meta tables :effects, characteristics, item-types
 #2 data tables :items, items set
 
-def run_sql_commands(sql_commands) :
+def run_sql_commands(sql_commands):
     cursor = sql_connection.cursor()
     for sql_command in sql_commands :
         cursor.execute(sql_command)
     sql_connection.commit()
 
+def fetch_spells():
+    for class_id in class_ids :
+        fetch_spells_for_class(class_id, False)
+
+def fetch_spells_for_class(class_id, force_image):
+    i = 0
+    cursor = sql_connection.cursor()
+
+    while True :
+        print(url_spells(i, class_id))
+        r = rq.get(url_spells(i, class_id))
+        if r.status_code != 200 :
+            break
+        js = r.json()
+        if not js["data"] :
+            break
+
+        for spell in js["data"] :
+            name = spell["name"]["fr"]
+            id = spell["id"]
+            class_id = spell["typeId"]
+            img_url = spell['img']
+
+            # 1- Obtient l'icone de l'item et sauvegarde localement.
+            file_path = '../../data/spells_img/' + str(id) + '.png'
+            if force_image or not os.path.exists(file_path):
+                imgdata = rq.get(img_url).content
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                with open(file_path, 'wb') as handler:
+                    handler.write(imgdata)
+            
+            cursor.execute(sql_insert_spells_command,
+                           (
+                               id,
+                               name,
+                               class_id
+                           ))
+        i += 1
+        
+    sql_connection.commit()
+
+def fetch_spell_levels():
+    for class_id in class_ids :
+        fetch_spell_levels_for_class(class_id)
+    
+def fetch_spell_levels_for_class(class_id):
+    i = 0
+    cursor = sql_connection.cursor()
+    
+    while True :
+        r = rq.get(url_spells_levels(i, class_id))
+        if r.status_code != 200 :
+            break
+        js = r.json()
+        if not js["data"] :
+            break
+        
+        for spell_level in js["data"] :
+            normal_array = []
+            crit_array = []
+            for normal_index in range(len(spell_level["effects"])) :
+                effect = spell_level["effects"][normal_index]
+                if effect["effectId"] in valid_effects:
+                    normal_array.append(effect)
+                    
+            for crit_index in range(len(spell_level["criticalEffect"])) :
+                effect = spell_level["criticalEffect"][crit_index]
+                if effect["effectId"] in valid_effects:
+                    crit_array.append(effect)
+                    
+            #--- Fix fleche de merde ---#
+            if spell_level["spellId"] == 13062 and spell_level["grade"] == 1:
+                normal_array[0]["effectId"]=96
+                normal_array[1]["effectId"]=97
+            if spell_level["spellId"] == 13062 and spell_level["grade"] == 2:
+                normal_array[0]["effectId"]=97
+            
+            spell_id = spell_level["spellId"]
+            crit_chance = spell_level["criticalHitProbability"]
+            level_requirement = spell_level["minPlayerLevel"]
+            
+            for shared_index in range(len(normal_array)):
+                effectId = normal_array[shared_index]["effectId"]
+                minVal = normal_array[shared_index]["diceNum"]
+                maxVal = normal_array[shared_index]["diceSide"]
+                minCritVal = crit_array[shared_index]["diceNum"] if crit_chance > 0 else normal_array[shared_index]["diceNum"]
+                maxCritVal = crit_array[shared_index]["diceSide"] if crit_chance > 0 else normal_array[shared_index]["diceSide"]
+                
+
+                cursor.execute(sql_insert_spell_levels_command,
+                               (
+                                   level_requirement,
+                                   effectId,
+                                   spell_id,
+                                   crit_chance, 
+                                   minVal, 
+                                   maxVal, 
+                                   minCritVal, 
+                                   maxCritVal)
+                               )
+            #Sanity check des effets normaux X crits
+            """
+            if crit_chance > 0:
+                for shared_index in range(len(normal_array)):
+                    if crit_array[shared_index]["effectId"] != normal_array[shared_index]["effectId"]:
+                        print(spell_level["spellId"])
+            """
+        i+=1
+        
+    sql_connection.commit()
+    
+            
 def fetch_effects() :
     i = 0
     cursor = sql_connection.cursor()
@@ -332,5 +471,9 @@ def complete_item_update() :
 
 if __name__== "__main__" :
     sql_connection = sql.connect(dbfile)
-    complete_item_update()
+    run_sql_commands([sql_drop_command("spells")])
+    run_sql_commands(sql_create_commands)
+    fetch_spells()
+    fetch_spell_levels()
+    
     sql_connection.close()
